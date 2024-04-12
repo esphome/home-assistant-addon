6d6769bb724a608914d878cd6ffd6fc486d2c25a
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-04-12 17:03:04 +1200

    Publish dev addon as versioned

--- script/generate.py
+++ script/generate.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
-import yaml
 from pathlib import Path
 from enum import Enum
 from shutil import copyfile
 import sys
 import os
+
+import yaml
 
 
 class Channel(Enum):
@@ -25,17 +26,16 @@ def main(args):
     root = Path(__file__).parent.parent
     templ = root / "template"
 
-    with open(templ / "addon_config.yaml", "r") as f:
+    with open(templ / "addon_config.yaml", "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
 
     copyf = config["copy_files"]
 
     for channel in args.channels:
         conf = config[f"esphome-{channel.value}"]
-        base_image = conf.pop("base_image", None)
         dir_ = root / conf.pop("directory")
         path = dir_ / "config.yaml"
-        with open(path, "w") as f:
+        with open(path, "w", encoding="utf-8") as f:
             yaml.dump(conf, f, indent=2, sort_keys=False, explicit_start=True)
 
         for file_ in copyf:
@@ -46,18 +46,8 @@ def main(args):
                 copyfile(templ / file_, dir_ / file_)
 
         path = dir_ / "FILES ARE GENERATED DO NOT EDIT"
-        with open(path, "w") as f:
+        with open(path, "w", encoding="utf-8") as f:
             f.write("Any edits should be made to the files in the 'template' directory")
-
-        if channel == Channel.dev:
-            path = dir_ / "build.yaml"
-            build_conf = {
-                "build_from": {
-                    arch: base_image for arch in conf["arch"]
-                }
-            }
-            with open(path, "w") as f:
-                yaml.dump(build_conf, f, indent=2, sort_keys=True, explicit_start=True)
 
 
 if __name__ == "__main__":
